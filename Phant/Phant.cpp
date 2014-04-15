38cc4cda53784c6ed57857ff5d862168587a1334
Todd Treece <toddtreece@gmail.com>
2014-04-15 12:35:00 -0400

    adds Phant.cpp

---
+++ Phant/Phant.cpp
@@ -0,0 +1,28 @@
+/**
+ * Phant.cpp
+ *
+ *             .-.._
+ *       __  /`     '.
+ *    .-'  `/   (   a \
+ *   /      (    \,_   \
+ *  /|       '---` |\ =|
+ * ` \    /__.-/  /  | |
+ *    |  / / \ \  \   \_\  jgs
+ *    |__|_|  |_|__\
+ *    never   forget.
+ *
+ * Author: Todd Treece <todd@sparkfun.com>
+ *
+ * Copyright (c) 2014 SparkFun Electronics.
+ * Licensed under the GPL v3 license.
+ *
+ */
+
+#include "Arduino.h"
+#include "Phant.h"
+
+Phant::Phant(String host, String publicKey, String privateKey) {
+  _host = host;
+  _pub = publicKey;
+  _prv = privateKey;
+}
